57649057a6610dea9dbdad12e8e0996ff5a1c81d
faalexandre <faustinoalfredoa@outlook.com>
2025-06-16 12:12:19 +0200

    Variables 02

---
+++ 002_1var_02/script.js
@@ -0,0 +1,16 @@
+// uma var podem ter um valor inicial e depois serem alteradas
+let myName = "Fausto";
+console.log(myName);
+
+let myAge = 35;
+
+console.log(myName + " " + myAge + "ans");
+
+const MAX_HEIGHT = 180;
+console.log("João Ribeiro tem" + " " + MAX_HEIGHT + "cm");
+
+var number1 = 5;
+var number2 = 7;
+var total = number1 + number2;
+
+console.log("O total das duas variaveis é:" + total);
---
+++ 004_strings/script.js
@@ -0,0 +1,20 @@
+// valores de string podem ser definidos usando as aspas simples ou duplas
+let myString1 = "Je suis au niveau de base en JS";
+console.log(myString1);
+
+//ou
+let myString2 = "Hello World!";
+console.log(myString2);
+
+//podemos usar crase (acento grave) para definir uma string
+let myString3 = `Je suis en mode superior`;
+console.log(myString3);
+
+//podemos usar aspas simples ou duplas dentro de uma string definida com crase:
+let fullName = `Jhon "Amazing" friend`;
+console.log(fullName);
+
+//podemos definir varias strings em uma unica linha, separando-as com virgulas
+let firstName = "Jhon",
+  lastName = "JLO";
+console.log(firstName + " " + lastName);
